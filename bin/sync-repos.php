a0226b37c8b4034e78da87de6232b385b2811dfc
webimpress <contact@webimpress.com>
2017-10-03 19:05:42 +0100

    Added script to synchronize repositories with zf-component-list.json
    It synchronizes only repositories listed in file
    https://docs.zendframework.com/zf-mkdoc-theme/scripts/zf-component-list.json
    
    It updates description, homepage (link to docs), disable wiki and projects tabs.

---
+++ bin/sync-repos.php
@@ -0,0 +1,38 @@
+#!/usr/bin/env php
+<?php
+
+use Github\Client;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+if (! isset($argv[1])) {
+    fwrite(STDERR, printf("Usage: php %s <github token>\n", basename(__FILE__)));
+    exit(1);
+}
+
+$list = json_decode(
+    file_get_contents('https://docs.zendframework.com/zf-mkdoc-theme/scripts/zf-component-list.json'),
+    true
+);
+
+$client = new Client();
+$client->authenticate($argv[1], null, $client::AUTH_URL_TOKEN);
+
+foreach ($list as $comp) {
+    $name = basename($comp['url']);
+    printf('Updating repository %s... ', $name);
+    try {
+        $client->repo()->update('zendframework', $name, [
+            'name'         => $name,
+            'description'  => $comp['description'],
+            'homepage'     => $comp['url'],
+            'has_projects' => false,
+            'has_wiki'     => false,
+        ]);
+        echo 'done';
+    } catch (\Throwable $e) {
+        printf('FAILED: %s', $e->getMessage());
+    }
+
+    echo PHP_EOL;
+}

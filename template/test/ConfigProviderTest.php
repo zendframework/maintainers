6fc39adc588d923d025ce9db851666ca8ac2793f
webimpress <contact@webimpress.com>
2018-02-13 09:26:20 +0000

    Added strict type declaration to template files

--- template/test/ConfigProviderTest.php
+++ template/test/ConfigProviderTest.php
@@ -5,6 +5,8 @@
  * @license   https://github.com/{org}/{repo}/blob/master/LICENSE.md New BSD License
  */
 
+declare(strict_types=1);
+
 namespace {namespace-test};
 
 use PHPUnit\Framework\TestCase;
